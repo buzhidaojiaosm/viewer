69a5234ca6175b98c96161f55572e9396db3004d
Kyra Yang <kyra.yang@silksoftware.com>
2023-05-30 17:01:10 +0800

    APPjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RepositoriesList from "./component/RepositoriesLists";
+import RepositoryDetails from "./component/RepositoryDetails";
 
-function App() {
+
+const App = () => {
+  const [username, setUsername] = useState("");
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>
+            <h1>Github Repositories Viewer</h1>
+            <input
+              placeholder="Enter Github Username"
+              value={username}
+              onChange={handleChange}
+            />
+            <RepositoriesList username={username} />
+          </div>} />
+          <Route path="/repo/:owner/:repoName" element={<RepositoryDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
+
 
 export default App;
